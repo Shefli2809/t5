7aed28ce7e10d81e97545fce8f92b121a5314395
Shefli2809 <90629203+Shefli2809@users.noreply.github.com>
2023-11-09 03:59:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AgeCalc from './components/AgeCalc';
+
+function App() {
+  return (
+    <div>
+      <AgeCalc />
+    </div>
+  );
+}
+
+export default App;
